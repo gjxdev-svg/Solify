e05e28356afe5883845d9d30864bafecad4c9903
gjxdev-svg <gjx.dev@gmail.com>
2026-07-20 21:20:18 -0400

    Starting Page (NOT FINALIZED)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <!-- The main container must wrap the view for Vue to render safely -->
+  <main>
+    <RouterView />
+  </main>
 </template>
 
 <style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue' // Double check this path!
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../login.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../register.vue'),
+    },
+  ],
 })
 
 export default router
