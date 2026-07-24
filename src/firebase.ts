be9d70ae6f5c08331fedcfcc810e1f818c908400
gjxdev-svg <gjx.dev@gmail.com>
2026-07-23 21:48:59 -0400

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider, type Auth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyCmD7SuYArSlQs_Z5pqjkAYr7sa9kmBjnA',
+  authDomain: 'solify1.firebaseapp.com',
+  projectId: 'solify1',
+  storageBucket: 'solify1.firebasestorage.app',
+  messagingSenderId: '467187591397',
+  appId: '1:467187591397:web:30fd8302a8ccd153394c0c',
+  measurementId: 'G-FGKFEZZY2W',
 }
 
 const app = initializeApp(firebaseConfig)
